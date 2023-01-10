cc2a655e3c4ef86c2b5ac4e0820b485bb3ca6843
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-01-10 21:44:39 +0100

    Solve day 3 task 1ˆ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,44 @@
 import okio.FileSystem
 import okio.Path.Companion.toPath
 
-var rucksacks: List<Pair<Compartment, Compartment>> = mutableListOf()
+var prioritySum = 0
 
 fun main(args: Array<String>) {
     val path = args[0].toPath()
 
     FileSystem.SYSTEM.read(path) {
         while (true) {
-            val line = readUtf8Line() ?: return
-            if (line.isBlank()) return
+            val line = readUtf8Line() ?: break
+            if (line.isBlank()) break
 
+            val halfLength = line.length / 2
+            val compartment1String = line.substring(0, halfLength)
+            val compartment2String = line.substring(halfLength)
 
+            val compartment1 = compartment1String.toSet()
+            val compartment2 = compartment2String.toSet()
+
+            var sharedItem: Char = '?'
+
+            run breaking@{
+                compartment1.forEach { char1 ->
+                    compartment2.find { it == char1 }?.let {
+                        sharedItem = it
+                        return@breaking
+                    }
+                }
+            }
+
+            println(line)
+            println("$compartment1String, $compartment2String")
+            println("$compartment1, $compartment2")
+            println(sharedItem)
+            println(sharedItem.priority)
+            prioritySum += sharedItem.priority
         }
+        println("Priority sum: $prioritySum")
     }
 }
-
-data class Compartment(
-    val items: List<Item>,
-)
 
 private const val uppercaseALetterCode = 'A'.code
 private const val uppercaseALetterPriority = 27
@@ -26,13 +46,9 @@ private const val uppercaseALetterPriority = 27
 private const val lowercaseALetterCode = 'a'.code
 private const val lowercaseALetterPriority = 1
 
-@JvmInline
-value class Item(val letter: Char) {
-
-    val priority: Int
-        get() = when (letter) {
-            in 'a'..'z' -> letter.code - lowercaseALetterCode + lowercaseALetterPriority
-            in 'A'..'Z' -> letter.code - uppercaseALetterCode + uppercaseALetterPriority
-            else -> throw IllegalArgumentException("char can only be in A..Za..z but was $letter")
-        }
-}
+val Char.priority: Int
+    get() = when (this) {
+        in 'a'..'z' -> code - lowercaseALetterCode + lowercaseALetterPriority
+        in 'A'..'Z' -> code - uppercaseALetterCode + uppercaseALetterPriority
+        else -> throw IllegalArgumentException("char can only be in A..Za..z but was $this")
+    }
